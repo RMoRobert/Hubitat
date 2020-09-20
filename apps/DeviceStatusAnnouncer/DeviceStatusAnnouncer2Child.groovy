8aff36d75e06ba1280e458604f46252ec1a7544d
RMoRobert <BertABCD1234@gmail.com>
2020-09-20 13:02:06 -0500

    DSA 2.5 release
    * Added thermostats as supported announceable device (if setpoints out of range)
    * Added contact/lock grouping for joint instead of individual announcements

--- apps/DeviceStatusAnnouncer/DeviceStatusAnnouncer2Child.groovy
+++ apps/DeviceStatusAnnouncer/DeviceStatusAnnouncer2Child.groovy
@@ -16,9 +16,10 @@
  *
  *  Author: Robert Morris
  *
- * == App version: 2.0.1 ==
+ * == App version: 2.5.0 ==
  *
  * Changelog:
+ * 2.5   (2020-09-20) - Added thermostats and contact/lock "name grouping"
  * 2.0.1 (2020-08-02) - Made easier to remove "all OK" notification/TTS if desired
  * 2.0   (2020-08-02) - New parent/child strucutre, additional notification options
  * 1.0   (2020-07-25) - First public release
@@ -31,7 +32,7 @@ definition(
    author: "Robert Morris",
    description: "Speak or notify status of locks, contact sensors, and other devices on demand",
    category: "Convenience",
-   parent: "RMoRobert:Device Status Announcer", // Comment out this line if 1.x upgrader or don't want to use parent
+   parent: "RMoRobert:Device Status Announcer", // Remove or comment out this line if 1.x upgrader or don't want to use parent
    iconUrl: "",
    iconX2Url: "",
    iconX3Url: "",
@@ -40,22 +41,54 @@ definition(
 
 preferences {
    page(name: "pageMain")
+   page(name: "pageRemoveGroup")
    page(name: "pageViewReport")
 }
 
 def pageMain() {
+   state.remove("removedGroup")
+   state.remove("removeGroup")
    dynamicPage(name: "pageMain", title: "Device Status Announcer", uninstall: true, install: true) {
       section(styleSection("Name")) {
          label title: "Name this Device Status Announcer:", required: true
       }
 
       section(styleSection("Choose Devices")) {
-         input name: "contactSensors", type: "capability.contactSensor", title: "Choose contact sensors:", multiple: true
+         input name: "contactSensors", type: "capability.contactSensor", title: "Choose contact sensors:", submitOnChange: true, multiple: true
          input name: "boolContactOpen", type: "bool", title: "Announce only if sensor(s) open", defaultValue: true
-         input name: "doorLocks", type: "capability.lock", title: "Choose locks:", multiple: true
+         input name: "doorLocks", type: "capability.lock", title: "Choose locks:", submitOnChange: true, multiple: true
          input name: "boolDoorUnlocked", type: "bool", title: "Announce only if lock(s) unlocked", defaultValue: true
          input name: "motionSensors", type: "capability.motionSensor", title: "Choose motion sensors:", multiple: true
          input name: "boolMotionActive", type: "bool", title: "Announce only if sensor(s) active", defaultValue: true
+         input name: "thermostats", type: "capability.thermostat", title: "Choose thermostats:", submitOnChange: true, multiple: true
+         if (settings["thermostats"]) {
+            input name: "thermostatCoolThreshold", type: "number", title: "if cooling setpoint below:", width: 6        
+            input name: "thermostatHeatThreshold", type: "number", title: "if heating setpoint above:", width: 6
+            paragraph "(heating and cooling setpoints will be evaluated only when thermostat in heating or cooling mode, respectively)"
+         }
+      }
+
+      if (contactSensors && doorLocks) {
+         section(styleSection("Naming/Grouping Devices")) {
+            List<Map<Long, String>> allContactsAndLocks = []
+            (contactSensors + doorLocks).each {
+               allContactsAndLocks.add([(it.deviceId): it.displayName])
+            }
+            paragraph "To group contact sensors and door locks into a joint announcement (e.g., \"Back door is open and unlocked\"), " +
+                     "choose the appropriate options below. (Do not add the same device to multiple groups.)"
+            if (state.contactLockGroups == null || state.contactLockGroups == []) state.contactLockGroups = [0]
+            (state.contactLockGroups).each {
+               input name: "contactLockGroup.${it}.devs", type: "enum", title: "Devices to group into single name:", multiple: true, submitOnChange: true,
+                  options: allContactsAndLocks, width: 5
+               if (settings["contactLockGroup.${it}.devs"]) {
+                  input name: "contactLockGroup.${it}.name", type: "text", title: "Name for this group:", width: 4
+                  href name: "contactLockGroup.${it}.hrefRemove", page: "pageRemoveGroup", title: "Remove", description: "", params: [groupNumber: it], width: 3
+               }
+            }
+            if (state.contactLockGroups?.size() > 0 && settings["contactLockGroup.${state.contactLockGroups[-1]}.name"] && settings["contactLockGroup.${state.contactLockGroups[-1]}.devs"]) {
+               input name: "btnNewContactLockGroup", type: "button", title: "New Group", submitOnChange: true
+            }
+         }
       }
 
       section(styleSection("Notification Options")) {
@@ -92,6 +125,22 @@ def pageMain() {
    }
 }
 
+def pageRemoveGroup(params) {
+   dynamicPage(name: "pageRemoveGroup", title: "Remove Group", uninstall: false, install: false, nextPage: "pageMain") {
+      if (params?.groupNumber != null) state.removeGroup = params.groupNumber
+      section("") {
+         if (state.removeGroup != null && state.removedGroup != true) {
+            paragraph "Press the button below to confirm removal of this group. Press \"Next\" to continue without removing."
+            input name: "contactLockGroup.${state.removeGroup}.btnRemove", type: "button", title: "Remove Group", submitOnChange: true
+         }
+         else {
+            if (state.removedGroup) paragraph "Group removed. Press \"Next\" to continue."
+            else paragraph "Unknown group removal status. Try again if it did not suceed. Press \"Next\" to continue."
+         }
+      }
+   }
+}
+
 def pageViewReport() {
    dynamicPage(name: "pageViewReport", title: "Device Status Announcer", uninstall: false, install: false, nextPage: "pageMain") {
       section(styleSection("Device Report")) {
@@ -110,30 +159,84 @@ String getDeviceStatusReport() {
    logDebug "Generating device status report..."
    List<String> statusReportList = []
    String statusReport = ""
-   contactSensors?.each {
+   com.hubitat.app.DeviceWrapperList contacts = settings["contactSensors"]
+   com.hubitat.app.DeviceWrapperList locks = settings["doorLocks"]
+   com.hubitat.app.DeviceWrapperList motions = settings["motionSensors"]
+   if (!(settings["boolIncludeDisabled"])) {
+      contacts = contacts?.findAll { it.isDisabled != true }
+      locks = locks?.findAll { it.isDisabled != true }
+      motions = motions?.findAll { it.isDisabled != true }
+   }
+   state.contactLockGroups?.each { groupId ->
+      if (settings["contactLockGroup.${groupId}.devs"] && settings["contactLockGroup.${groupId}.name"]) {
+         List<String> groupState = []
+         if (boolContactOpen != false && boolDoorUnlocked != false) {
+            contacts.find { it.id in settings["contactLockGroup.${groupId}.devs"] && it.currentValue("contact") == "open" }?.each {
+               groupState << "open"
+               contacts.remove(it)
+            }
+            locks.find { it.id in settings["contactLockGroup.${groupId}.devs"] && it.currentValue("lock") == "unlocked" }?.each {
+               groupState << "unlocked"
+               locks.remove(it)
+            }
+         }
+         else {
+            contacts.find { it.id in settings["contactLockGroup.${groupId}.devs"]}?.each {
+               groupState << it.currentValue("contact")
+               contacts.remove(it)
+            }
+            locks.find { it.id in settings["contactLockGroup.${groupId}.devs"] }?.each {
+               groupState << it.currentValue("lock")
+               locks.remove(it)
+            }
+         }
+         if (groupState.size() > 0) {
+            statusReportList << """${settings["contactLockGroup.${groupId}.name"]} is ${groupState.join(" and ")}"""
+         }
+      }
+   }
+   contacts?.each {
       if (boolContactOpen != false) {
-         if (it.currentValue("contact") == "open" && (it.isDisabled() == false || settings["boolIncludeDisabled"])) statusReportList << "${it.displayName} is open"
+         if (it.currentValue("contact") == "open") statusReportList << "${it.displayName} is open"
       }
       else {
-          if (it.isDisabled() == false || settings["boolIncludeDisabled"]) statusReportList << "${it.displayName} is ${it.currentValue("contact")}"
+         statusReportList << "${it.displayName} is ${it.currentValue("contact")}"
       }
    }
-   doorLocks?.each {
+   locks?.each {
       if (boolDoorUnlocked != false) {
-         if (it.currentValue("lock") == "unlocked" && (it.isDisabled() == false || settings["boolIncludeDisabled"]))  statusReportList << "${it.displayName} is unlocked"
+         if (it.currentValue("lock") == "unlocked")  statusReportList << "${it.displayName} is unlocked"
       }
       else {
-          if (it.isDisabled() == false || settings["boolIncludeDisabled"]) statusReportList << "${it.displayName} is ${it.currentValue("lock")}"
+         statusReportList << "${it.displayName} is ${it.currentValue("lock")}"
       }
    }
-   motionSensors?.each {
+   motions?.each {
       if (boolMotionActive != false) {
-         if (it.currentValue("motion") == "active" && (it.isDisabled() == false || settings["boolIncludeDisabled"]))  statusReportList << "${it.displayName} is active"
+         if (it.currentValue("motion") == "active")  statusReportList << "${it.displayName} is active"
       }
       else {
-          if (it.isDisabled() == false || settings["boolIncludeDisabled"]) statusReportList << "${it.displayName} is ${it.currentValue("motion")}"
+         statusReportList << "${it.displayName} is ${it.currentValue("motion")}"
       }
    }
+   thermostats?.each {
+      if ((it.currentValue("thermostatMode") == "cool" ||
+          it.currentValue("thermostatMode") == "auto") &&
+          (settings["thermostatCoolThreshold"] != null &&
+          it.currentValue("coolingSetpoint") < settings["thermostatCoolThreshold"]))
+      {
+         statusReportList << "${it.displayName} is set to ${it.currentValue('coolingSetpoint')}"
+      }
+      else if ((it.currentValue("thermostatMode") == "heat" ||
+               it.currentValue("thermostatMode") == "emergency heat" ||
+               it.currentValue("thermostatMode") == "auto") &&
+               (settings["thermostatCoolThreshold"] != null &&
+               it.currentValue("heatingSetpoint") > settings["thermostatHeatThreshold"]))
+      {
+         statusReportList << "${it.displayName} is set to ${it.currentValue('heatingSetpoint')}"
+      }
+   }
+
    if (statusReportList.size() >= 2) {
       statusReport = statusReportList[0..-2].join(", ") + ", and " + statusReportList[-1]
    }
@@ -202,6 +305,44 @@ String styleSection(String sectionHeadingText) {
    return """<div style="font-weight:bold; font-size: 120%">$sectionHeadingText</div>"""
 }
 
+Boolean isModeOK() {
+    Boolean isOK = !settings["modes"] || settings["modes"].contains(location.mode)
+    logDebug "Checking if mode is OK; returning: ${isOK}"
+    return isOK
+}
+
+private void removeContactLockGroup(String groupNum) {
+   logDebug "removeContactLockGroup($groupNum)"
+	def settingNamesToRemove = settings?.keySet()?.findAll{ it.startsWith("contactLockGroup.${groupNum}") }
+	logDebug "  Settings to remove: $settingNamesToRemove"
+	settingNamesToRemove.each { settingName ->
+		app.removeSetting(settingName)
+	}
+   state.contactLockGroups.removeElement(groupNum as Integer)
+   state.remove('removeGroup')
+   state.removedGroup = true
+   logDebug "Finished removing contact lock group $groupNum"
+}
+
+def appButtonHandler(btn) {
+   switch (btn) {
+      case "btnTestNotification":
+         doNotificationOrAnnouncement()
+         break
+      case "btnNewContactLockGroup":
+			if (state.contactLockGroups == null) state.contactLockGroups = [0]
+			Integer newMaxGroup = (state.contactLockGroups?.size() > 0) ? ((state.contactLockGroups[-1] as Integer) + 1) : 0
+         state.contactLockGroups << newMaxGroup
+         break
+      case { it.startsWith("contactLockGroup.") }:
+         String clGrpNum = btn - "contactLockGroup." - ".btnRemove"
+         removeContactLockGroup(clGrpNum)
+         break
+      default:
+         log.debug "Unhandled button press: $btn"
+   }
+}
+
 //=========================================================================
 // App Methods
 //=========================================================================
@@ -227,22 +368,6 @@ def initialize() {
    if (settings["notificationTime"]) schedule(settings["notificationTime"], scheduleHandler) 
    if (settings["announcementSwitch"]) subscribe(settings["announcementSwitch"], "switch", switchHandler)
    if (settings["sensorAway"]) subscribe(settings["sensorAway"], "presence", presenceAwayHandler)
-}
-
-Boolean isModeOK() {
-    Boolean isOK = !settings["modes"] || settings["modes"].contains(location.mode)
-    logDebug "Checking if mode is OK; returning: ${isOK}"
-    return isOK
-}
-
-def appButtonHandler(btn) {
-   switch (btn) {
-      case "btnTestNotification":
-         doNotificationOrAnnouncement()
-         break
-      default:
-         log.debug "Unhandled button press: $btn"
-   }
 }
 
 /** Writes to log.debug if debug logging setting enabled
