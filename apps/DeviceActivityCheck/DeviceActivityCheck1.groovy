ebd85721fbb4744f7bd7504811e0c4501a01df6b
RMoRobert <BertABCD1234@gmail.com>
2020-12-17 23:08:22 -0600

    Device Activity Check 1.3
    * Option to refresh selected devices before report
    * Default date/time formatting on "View current report"
    * Ability to choose multiple notification devices

--- apps/DeviceActivityCheck/DeviceActivityCheck1.groovy
+++ apps/DeviceActivityCheck/DeviceActivityCheck1.groovy
@@ -16,11 +16,11 @@
  *
  *  Author: Robert Morris
  *
- * == App version: 1.2.0 ==
- *
  * Changelog:
- * 1.2.0 (2020-07-25) - Added presence checking (in addition to activity)
- * 1.1.0 (2020-06-11) - Added ability to ignore disabled devices (on by default)
+ * 1.3   (2020-12-18) - Added ability to refresh selected devices before report, ability to select multiple notification
+ *                      devices, and better default date/time formatting on "View current report" page
+ * 1.2   (2020-07-28) - Added presence checking (in addition to activity)
+ * 1.1   (2020-06-11) - Added ability to ignore disabled devices (on by default)
  * 1.0.1 (2020-06-04) - Minor bugfix (eliminates errors for empty groups or if notification device is not selected)
  * 1.0   (2020-05-27) - First public release
  *
@@ -28,7 +28,7 @@
 
  import groovy.transform.Field
 
-@Field static List dateFormatOptions = ['MMM d, yyyy, h:mm a', 'E, MMM d, yyyy, h:mm a', 'E dd MMM yyyy, h:mm a', 'dd MMM yyyy, h:mm a', ,
+@Field static List dateFormatOptions = ['MMM d, yyyy, h:mm a', 'E, MMM d, yyyy, h:mm a', 'E dd MMM yyyy, h:mm a', 'dd MMM yyyy, h:mm a',
                                         'dd MMM yyyy HH:mm', 'E MMM dd HH:mm', 'yyyy-MM-dd HH:mm z']
 @Field static Integer formatListIfMoreItemsThan = 4
 
@@ -76,14 +76,14 @@ def pageMain() {
 					 params: [groupNumber: realGroupNum, groupDispNumber: groupIndex+1],
 					 title: strTitle,
 					 description: getDeviceGroupDescription(realGroupNum) ?: "Click/tap to choose devices and inactivity threshold...",
-					 state: getDeviceGroupDescription(realGroupNum) ? 'complete' : null)
+					 state: getDeviceGroupDescription(realGroupNum) ? "complete" : null)
 				}
 			paragraph("To monitor another set of devices with a different inactiviy threshold or method, add a new group:")
 			input name: "btnNewGroup", type: "button", title: "Add new group"
 		}
 
 		section(styleSection("Notification Options")) {
-			input name: "notificationDevice", type: "capability.notification", title: "Send notification with list of inactive devices to this device:"
+			input name: "notificationDevice", type: "capability.notification", title: "Send notification with list of inactive devices to this device:", multiple: true
 			input name: "notificationTime", type: "time", title: "Daily at this time:"
 			paragraph "Or any time this switch is turned on:"
 			input name: "notificationSwitch", type: "capability.switch", title: "Switch", description: "Optional - Click to set"
@@ -97,7 +97,7 @@ def pageMain() {
 					timeFormatOptions << ["$it": "${currDate.format(it, location.timeZone)}"]
 				}
 				input name: "timeFormat", type: "enum", options: timeFormatOptions, title: "Date/time format for notifications:",
-					defaultValue: timeFormatOptions[0]?.keySet()[0], required: (!settings['includeTime'] == false), submitOnChange: true
+					defaultValue: timeFormatOptions[0]?.keySet()[0], required: (!settings["includeTime"] == false), submitOnChange: true
 			}
 		}
 
@@ -113,8 +113,9 @@ def pageMain() {
 		section("Advanced Options", hideable: true, hidden: true) {
          label title: "Customize installed app name:", required: true
          input name: "includeHubName", type: "bool", title: "Include hub name in reports (${location.name})"
-         input name: "useNotificationTimeFormatForReport", type: "bool", title: 'Use "Date/time format for notifications" for "View current report" dates/times'
-         input "modes", "mode", title: "Only send notifications when mode is", multiple: true, required: false
+         input name: "useNotificationTimeFormatForReport", type: "bool", title: 'Use "Date/time format for notifications" for "View current report" dates/times (default is YYYY-MM-dd hh:mm a z)'
+         input name: "refreshBeforeViewReport", type: "bool", title: "If configured to refresh devices before sending inactivity notification, also refresh before \"View current report\" (default is no; enabling may cause delay loading that page)"
+         input name: "modes", type: "mode", title: "Only send notifications when mode is", multiple: true, required: false
          input name: "boolIncludeDisabled", type: "bool", title: "Include disabled devices in report"
          input name: "debugLevel", type: "enum", title: "Debug logging level:", options: [[0: "Logs off"], [1: "Debug logging"], [2: "Verbose logging"]],
             defaultValue: 0
@@ -141,7 +142,7 @@ def pageDeviceGroup(params) {
       groupNum = state.currGroupDispNum
    }
 	strTitle = (state.groups?.size() > 1) ? "Device Group ${groupDispNum}:" : "Devices"   
-	state.remove('cancelDelete')
+	state.remove("cancelDelete")
 
    dynamicPage(name: "pageDeviceGroup", title: strTitle, uninstall: false, install: false, nextPage: "pageMain") {
       section(styleSection("Choose Devices")) {
@@ -179,6 +180,21 @@ def pageDeviceGroup(params) {
             paragraph "Please select a valid \"inactivity detection method\" option above."
          }
       }
+      if (settings["group${groupNum}.devices"] && (settings["group${groupNum}.inactivityMethod"] == "activity" || settings["group${groupNum}.inactivityMethod"] == null)) {
+         section(styleSection("Refresh Options")) {
+            List<Map<Long, String>> rdevList = []
+            settings["group${groupNum}.devices"].each {
+               if (it.hasCommand("refresh")) rdevList << [(it.getIdAsLong()): it.getDisplayName()]
+            }
+            // Sort by device display name:
+            rdevList.sort { a, b -> a.entrySet().iterator().next()?.value <=> b.entrySet().iterator().next()?.value }
+            paragraph "Use the option below only if you have devices that do not regularly \"check in\" and respond to a refresh. (You must know your specific device behavior, but generally all powered devices and battery-powered Zigbee devices will respond, while non-FLiRS Z-Wave battery devices will not.) Device Activity Check will send a \"Refresh\" command to the device before running a report, which may give a better indication of whether the device is still responsive. Be careful not to choose too many devices or devices that do not respond on-demand to refresh commands."
+            input name: "group${groupNum}.refreshDevs", title: "Refresh before running report:", type: "enum", options: rdevList, multiple: true, submitOnChange: true
+            if (settings["group${groupNum}.refreshDevs"]) {
+               paragraph "<strong>Note:</strong> If any devices need to be refreshed, reports will be delayed for one-half second per device plus an additional five seconds in order to give devices time to respond (so the \"Test\" on the previous page may not be instant)."
+            }
+         }
+      }
       section(styleSection("Remove Group")) {
          href(name: "pageRemoveGroupHref",
               page: "pageRemoveGroup",
@@ -193,7 +209,7 @@ def pageRemoveGroup(params) {
    logDebug("pageRemoveGroup with parameters $params...")
    Integer groupNum = params?.deleteGroupNumber	
    if (groupNum && !(state.cancelDelete)) {
-      state.remove('lastGroupNum')
+      state.remove("lastGroupNum")
       state.removeSettingsForGroupNumber = groupNum
    }
    dynamicPage(name: "pageRemoveGroup", title: "Remove Group", uninstall: false, install: false, nextPage: "pageMain") {
@@ -210,22 +226,23 @@ def pageRemoveGroup(params) {
 }
 
 def pageViewReport() {
+   logDebug "Loading \"View current report\" page... (if you have device refresh enabled, this may take several seconds)"
 	dynamicPage(name: "pageViewReport", title: "Device Activity Check", uninstall: false, install: false, nextPage: "pageMain") {
 		section(styleSection("Inactive Device Report")) {
-			List<com.hubitat.app.DeviceWrapper> inactiveDevices = getInactiveDevices()
+			List<com.hubitat.app.DeviceWrapper> inactiveDevices = getInactiveDevices(true, settings["refreshBeforeViewReport"] ?: false)
 			if (inactiveDevices) {
 				paragraph "<strong>Device</strong>", width: 6
 				paragraph "<strong>Last Activity</strong>", width: 6
 				Boolean doFormatting = inactiveDevices.size() > formatListIfMoreItemsThan
 				inactiveDevices.eachWithIndex { dev, index ->
 					String lastActivity
-					if (settings['useNotificationTimeFormatForReport']) {
-						lastActivity = dev.getLastActivity()?.format(settings['timeFormat'] ?: 'MMM dd, yyyy h:mm a', location.timeZone)
+					if (settings["useNotificationTimeFormatForReport"]) {
+						lastActivity = dev.getLastActivity()?.format(settings["timeFormat"] ?: "MMM dd, yyyy h:mm a", location.timeZone)
 					}
-					else {
-						lastActivity = dev.getLastActivity()?.toString()
-					}
-					if (!lastActivity) lastActivity = 'No reported activity'
+               else {                  
+						lastActivity = dev.getLastActivity()?.format("yyyy-MM-dd hh:mm a z", location.timeZone)
+               }
+					if (!lastActivity) lastActivity = "No reported activity"
 					paragraph(doFormatting ? "${styleListItem(dev.displayName, index)}" : "${dev.displayName}", width: 6)
 					paragraph(doFormatting ? "${styleListItem(lastActivity, index)}" : lastActivity, width: 6)
 				}
@@ -237,10 +254,11 @@ def pageViewReport() {
 	}
 }
 
-List<com.hubitat.app.DeviceWrapper> getInactiveDevices(Boolean sortByName=true) {
+List<com.hubitat.app.DeviceWrapper> getInactiveDevices(Boolean sortByName=true, Boolean refreshSelectedDevices=false) {
    logDebug "getInactiveDevices()...", "trace"
 	List<Integer> groups = state.groups ?: [1]
 	List<com.hubitat.app.DeviceWrapper> inactiveDevices = []
+	List<com.hubitat.app.DeviceWrapper> toRefreshDevices = []
 	Long currEpochTime = now()
    Closure inactivityDetectionClosure
    Closure disabledCheckClosure = { com.hubitat.app.DeviceWrapper dev -> !(dev.isDisabled()) || !(settings["boolIncludeDisabled"]) }
@@ -256,6 +274,9 @@ List<com.hubitat.app.DeviceWrapper> getInactiveDevices(Boolean sortByName=true) 
             disabledCheckClosure(dev)
          }
          inactivityDetectionClosure = inactivityDetectionClosure.curry(cutoffEpochTime)
+         if (settings["group${groupNum}.refreshDevs"] && (settings["group${groupNum}.inactivityMethod"] == "activity" || settings["group${groupNum}.inactivityMethod"] == null)) {
+            toRefreshDevices += settings["group${groupNum}.devices"]?.findAll { settings["group${groupNum}.refreshDevs"].contains (it.getId()) }
+         }
       }
       // For presence-based devices:
       else if (settings["group${groupNum}.inactivityMethod"] == "presence") {
@@ -272,12 +293,25 @@ List<com.hubitat.app.DeviceWrapper> getInactiveDevices(Boolean sortByName=true) 
       inactiveDevices.addAll(allDevices?.findAll(inactivityDetectionClosure))
 	}
 	if (sortByName) inactiveDevices = inactiveDevices.sort { it.displayName }
+   if (refreshSelectedDevices && toRefreshDevices) {
+      List<com.hubitat.app.DeviceWrapper> toActuallyRefresh = toRefreshDevices.intersect(inactiveDevices)
+      logDebug "Devices to refresh: $toActuallyRefresh"
+      toActuallyRefresh.each {
+         try {
+            it.refresh()
+            pauseExecution(400)
+         }
+         catch (Exception ex) {
+            log.warn "Could not refresh $it; $ex"
+         }
+      }
+      pauseExecution(5000 + toActuallyRefresh.size() * 100) // Give devices time to respond
+   }
    inactivityDetectionClosure = null
    disabledCheckClosure = null
    logDebug "getInactiveDevices() returning: $inactiveDevices", "trace"
 	return inactiveDevices
 }
-
 
 // Lists all devices in group, one per line
 String getDeviceGroupDescription(groupNum) {
@@ -338,29 +372,30 @@ String styleSection(String sectionHeadingText) {
 }
 
 void switchHandler(evt) {
-	if (evt.value == 'on') {
+	if (evt.value == "on") {
 		logDebug("Switch turned on; running report")
 		sendInactiveNotification()
 	}
 }
 
 // Sends notification with list of inactive devices to selected notification device(s)
-void sendInactiveNotification(Boolean includeLastActivityTime=(settings['includeTime'] != false)) {
-   log.trace "--"
+void sendInactiveNotification(Boolean includeLastActivityTime=(settings["includeTime"] != false)) {
 	logDebug "sendInactiveNotification() called...preparing list of inactive devices."
-	List<com.hubitat.app.DeviceWrapper> inactiveDevices = getInactiveDevices() 
+	List<com.hubitat.app.DeviceWrapper> inactiveDevices = getInactiveDevices(true, true) 
 	String notificationText = ""
 	if (inactiveDevices && isModeOK()) {
-		notificationText += (settings['includeHubName'] ? "${app.label} - ${location.name}:" : "${app.label}:")		
+		notificationText += (settings["includeHubName"] ? "${app.label} - ${location.name}:" : "${app.label}:")		
 		inactiveDevices.each { dev ->
 			notificationText += "\n${dev.displayName}"
 			if (includeLastActivityTime) {
-				String dateString = dev.getLastActivity()?.format(settings['timeFormat'] ?: 'MMM dd, yyyy h:mm a', location.timeZone) ?: 'No activity reported'
+				String dateString = dev.getLastActivity()?.format(settings["timeFormat"] ?: 'MMM dd, yyyy h:mm a', location.timeZone) ?: 'No activity reported'
 				notificationText += " - $dateString"
 			}
 		}		
 		logDebug "Sending notification for inactive devices: \"$notificationText\""
-		notificationDevice?.deviceNotification(notificationText)
+		notificationDevice?.each {
+         it.deviceNotification(notificationText)
+      }
 	}
 	else {
 		String reason = "Notification skipped: "
@@ -397,17 +432,17 @@ def updated() {
 
 def initialize() {
 	log.trace "Initialized"
-	if (settings['debugLevel']) {
+	if (settings["debugLevel"] && settings["debugLevel"] as Integer != 0) {
 		log.debug "Debug logging is enabled for ${app.label}. It will remain enabled until manually disabled."
 	}
 
 	unsubscribe()
-	if (settings['notificationTime']) schedule(settings['notificationTime'], scheduleHandler)	
-	if (settings['notificationSwitch']) subscribe(settings['notificationSwitch'], 'switch', switchHandler)
+	if (settings["notificationTime"]) schedule(settings["notificationTime"], scheduleHandler)	
+	if (settings["notificationSwitch"]) subscribe(settings["notificationSwitch"], "switch", switchHandler)
 }
 
 Boolean isModeOK() {
-    Boolean isOK = !settings['modes'] || settings['modes'].contains(location.mode)
+    Boolean isOK = !settings["modes"] || settings["modes"].contains(location.mode)
     logDebug "Checking if mode is OK; reutrning: ${isOK}", "trace"
     return isOK
 }
@@ -420,7 +455,7 @@ def appButtonHandler(btn) {
 			break
 		case "btnCancelGroupDelete":
 			state.cancelDelete = true
-			state.remove('removeSettingsForGroupNumber')
+			state.remove("removeSettingsForGroupNumber")
 			break
 		case "btnTestNotification":
 			sendInactiveNotification()
