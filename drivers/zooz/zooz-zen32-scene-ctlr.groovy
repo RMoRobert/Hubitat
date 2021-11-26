3d3621e024894382d6a1050512767fc0d84d838e
Jeff Page <jtp10181@gmail.com>
2021-11-25 21:18:10 -0600

    Add indicator command support
    Also upgraded command class version, and other minor cleanup

--- drivers/zooz/zooz-zen32-scene-ctlr.groovy
+++ drivers/zooz/zooz-zen32-scene-ctlr.groovy
@@ -43,18 +43,18 @@
 import groovy.transform.Field
 
 @Field static final Map commandClassVersions = [
-   0x20: 1,    // Basic
-   0x25: 1,    // SwitchBinary
-   0x55: 1,    // TransportService
-   0x59: 1,    // AssociationGroupInfo
-   0x5B: 1,    // CentralScene
+   0x20: 2,    // Basic
+   0x25: 2,    // SwitchBinary
+   0x55: 2,    // TransportService
+   0x59: 3,    // AssociationGroupInfo
+   0x5B: 3,    // CentralScene
    0x6C: 1,    // Supervision
-   0x70: 1,    // Configuration
+   0x70: 2,    // Configuration
    0x72: 2,    // ManufacturerSpecific
-   0x85: 1,    // Association
-   0x86: 2,    // Version
-   0x8E: 2,    // MultichannelAssociation
-   0x98: 1,    // Security
+   0x85: 3,    // Association
+   0x86: 3,    // Version
+   0x87: 3,    // Indicator
+   0x8E: 4,    // MultichannelAssociation
    0x9F: 1     // Security S2
 ]
 
@@ -121,8 +121,13 @@ metadata {
       // Uncomment if switching from another driver and need to "clean up" things--will expose command in UI:
       //command "clearChildDevsAndState"
 
-      fingerprint mfr:"027A", prod:"7000", deviceId:"A008", inClusters:"0x5E,0x25,0x70,0x20,0x5B,0x85,0x8E,0x59,0x55,0x86,0x72,0x5A,0x73,0x87,0x9F,0x6C,0x7A" 
-      fingerprint mfr:"027A", prod:"7000", model: "A008"
+      //command "indicatorGet" //, [[name:"ledNumber", type: "NUMBER", description: "LED/button number (1-5, 5=large/relay button)", constraints: 1..5] ]
+      command "indicatorSet", [[name:"Mode*", type: "ENUM", description: "Select the mode", constraints: ["Flash", "On", "Off"]],
+                         [name:"ledNumber*", type: "NUMBER", description: "LED/Button number (1-5, 5=large button, 0=all)", constraints: 0..5],
+                         [name:"flashSettings", type: "STRING", description: "Format: 1,2,3 : 1-On/Off period length in tenths of seconds, 2-Number of On/Off periods, 3-On time in tenths of seconds (asymmetric periods)"],
+                        ]
+
+      fingerprint mfr:"027A", prod:"7000", deviceId:"A008"
    }
 
    preferences {
@@ -135,6 +140,44 @@ metadata {
       input name: "enableDesc", type: "bool", title: "Enable descriptionText logging", defaultValue: true
    }
 }
+
+List<String> indicatorGet() {
+   List<String> cmds = []
+   cmds << zwaveSecureEncap(zwave.indicatorV3.indicatorSupportedGet(indicatorId:0x00))
+   return delayBetween(cmds,400)
+}
+
+List<String> indicatorSet(String mode, Number ledNumber=0, String indSettings=null) {
+   Map indMap = [0:0x50, 5:0x43, 1:0x44, 2:0x45, 3:0x46, 4:0x47]
+   Map propValues = [3:2, 4:10, 5:0]
+   Number indID = indMap[ledNumber as Integer]
+   if (indSettings) {
+      String[] setSplit = indSettings.split(',')
+      //log.debug "indicatorSet: ${setSplit}, ${setSplit.size()}"
+      for (int idx=0; idx < setSplit.size(); idx++) {
+         if (setSplit[idx]) { propValues[idx+3] = setSplit[idx] as Integer }
+      }
+   }
+
+   //log.debug "indicatorSet: ${ledNumber}, ${indSettings}, ${indID}, ${propValues}"
+   List<String> cmds = []
+   if (mode.equalsIgnoreCase("flash")) {
+      cmds << zwaveSecureEncap(zwave.indicatorV3.indicatorSet(value:0xFF, indicatorCount:3, indicatorValues:[
+            [indicatorId:indID, propertyId:3, value:propValues[3]], //This property is used to set the duration in tenth of seconds of an On/Off period.
+            [indicatorId:indID, propertyId:4, value:propValues[4]], //This property is used to set the number of On/Off periods to run
+            [indicatorId:indID, propertyId:5, value:propValues[5]] //This property is used to set the length of the On time during an On/Off period. It allows asymetic On/Off periods.
+         ]))
+   }
+   else {
+      def onOff = (mode.equalsIgnoreCase("on") ? 0xFF : 0x00)
+      cmds << zwaveSecureEncap(zwave.indicatorV3.indicatorSet(value:0xFF, indicatorCount:1, indicatorValues:[
+            [indicatorId:indID, propertyId:2, value:onOff]
+         ]))
+   }
+   
+   return delayBetween(cmds,400)
+}
+
 
 void logsOff() {
    log.warn "Disabling debug logging"
@@ -156,7 +199,7 @@ void zwaveEvent(hubitat.zwave.commands.supervisionv1.SupervisionGet cmd){
    }
 }
 
-void zwaveEvent(hubitat.zwave.commands.versionv2.VersionReport cmd) {
+void zwaveEvent(hubitat.zwave.commands.versionv3.VersionReport cmd) {
    if (logEnable) log.debug "VersionReport: ${cmd}"
    device.updateDataValue("firmwareVersion", """${cmd.firmware0Version}.${String.format("%02d", cmd.firmware0SubVersion)}""")
    device.updateDataValue("protocolVersion", "${cmd.zWaveProtocolVersion}.${cmd.zWaveProtocolSubVersion}")
@@ -180,33 +223,33 @@ void zwaveEvent(hubitat.zwave.commands.manufacturerspecificv2.DeviceSpecificRepo
    }
 }
 
-void zwaveEvent(hubitat.zwave.commands.configurationv1.ConfigurationReport cmd) {
+void zwaveEvent(hubitat.zwave.commands.configurationv2.ConfigurationReport cmd) {
    if (enableDebug) log.debug "ConfigurationReport: ${cmd}"
    if (enableDesc) log.info "${device.displayName} parameter '${cmd.parameterNumber}', size '${cmd.size}', is set to '${cmd.scaledConfigurationValue}'"
 }
 
-void zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd) {
+void zwaveEvent(hubitat.zwave.commands.basicv2.BasicReport cmd) {
    if (enableDebug) log.debug "BasicReport:  ${cmd}"
    String value = (cmd.value ? "on" : "off")
    if (enableDesc && device.currentValue("switch") != value) log.info "${device.displayName} switch is ${value}"
    sendEvent(name: "switch", value: value)
 }            
 
-void zwaveEvent(hubitat.zwave.commands.basicv1.BasicSet cmd) {
-   if (enableDebug) log.debug "BasicSet: ${cmd}"
-   String value = (cmd.value ? "on" : "off")
-   if (enableDesc && device.currentValue("switch") != value) log.info "${device.displayName} switch is ${value}"
-   sendEvent(name: "switch", value: value)
-}
+// void zwaveEvent(hubitat.zwave.commands.basicv2.BasicSet cmd) {
+//    if (enableDebug) log.debug "BasicSet: ${cmd}"
+//    String value = (cmd.value ? "on" : "off")
+//    if (enableDesc && device.currentValue("switch") != value) log.info "${device.displayName} switch is ${value}"
+//    sendEvent(name: "switch", value: value)
+// }
 
-void zwaveEvent(hubitat.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
+void zwaveEvent(hubitat.zwave.commands.switchbinaryv2.SwitchBinaryReport cmd) {
    if (enableDebug) log.debug "SwitchBinaryReport: ${cmd}"
    String value = (cmd.value ? "on" : "off")
    if (enableDesc && device.currentValue("switch") != value) log.info "${device.displayName} switch is ${value}"
    sendEvent(name: "switch", value: value)
 }
 
-void zwaveEvent(hubitat.zwave.commands.centralscenev1.CentralSceneNotification cmd) {    
+void zwaveEvent(hubitat.zwave.commands.centralscenev3.CentralSceneNotification cmd) {    
    if (enableDebug) log.debug "CentralSceneNotification: ${cmd}"
    Integer btnBaseNum = cmd.sceneNumber ?: 0
    Integer btnNum = btnBaseNum
@@ -224,8 +267,15 @@ void zwaveEvent(hubitat.zwave.commands.centralscenev1.CentralSceneNotification c
    }
 }
 
-void zwaveEvent(hubitat.zwave.commands.indicatorv1.IndicatorReport cmd) {
+void zwaveEvent(hubitat.zwave.commands.indicatorv3.IndicatorReport cmd) {
    if (enableDebug) log.debug "IndicatorReport: ${cmd}"
+}
+
+void zwaveEvent(hubitat.zwave.commands.indicatorv3.IndicatorSupportedReport cmd) {
+   if (enableDebug) log.debug "IndicatorSupportedReport: ${cmd}"
+   if (cmd.nextIndicatorId > 0) {
+      sendHubCommand(new hubitat.device.HubAction(zwaveSecureEncap(zwave.indicatorV3.indicatorSupportedGet(indicatorId:cmd.nextIndicatorId)), hubitat.device.Protocol.ZWAVE)) 
+   }
 }
 
 void zwaveEvent(hubitat.zwave.Command cmd){
@@ -235,22 +285,22 @@ void zwaveEvent(hubitat.zwave.Command cmd){
 List<String> refresh() {
    if (enableDebug) log.debug "refresh"
    return delayBetween([
-      zwaveSecureEncap(zwave.basicV1.basicGet()),
+      zwaveSecureEncap(zwave.basicV2.basicGet()),
       zwaveSecureEncap(zwave.configurationV1.configurationGet()),
-      zwaveSecureEncap(zwave.versionV2.versionGet())
+      zwaveSecureEncap(zwave.versionV3.versionGet())
    ], 100)
 }
 
 String on() {
    if (enableDebug) log.debug "on()"
    state.flashing = false
-   zwaveSecureEncap(zwave.basicV1.basicSet(value: 0xFF))
+   zwaveSecureEncap(zwave.basicV2.basicSet(value: 0xFF))
 }
 
 String off() {
    if (enableDebug) log.debug "off()"
    state.flashing = false
-   zwaveSecureEncap(zwave.basicV1.basicSet(value: 0x00))
+   zwaveSecureEncap(zwave.basicV2.basicSet(value: 0x00))
 }
 
 void push(btnNum) {
@@ -293,8 +343,7 @@ List<String> configure() {
       cmds << zwaveSecureEncap(zwave.configurationV1.configurationSet(scaledConfigurationValue: val, parameterNumber: 1, size: 1))
    }
 
-   cmds << zwaveSecureEncap(zwave.versionV2.versionGet())
-   cmds << zwaveSecureEncap(zwave.versionV2.versionGet())
+   cmds << zwaveSecureEncap(zwave.versionV3.versionGet())
    cmds << zwaveSecureEncap(zwave.configurationV1.configurationGet())
    return delayBetween(cmds, 150)
 }
@@ -323,16 +372,10 @@ List<String> updated() {
       cmds << zwaveSecureEncap(zwave.configurationV1.configurationSet(scaledConfigurationValue: val, parameterNumber: 1, size: 1))
    }
 
-   cmds << zwaveSecureEncap(zwave.versionV2.versionGet())
+   cmds << zwaveSecureEncap(zwave.versionV3.versionGet())
    cmds << zwaveSecureEncap(zwave.manufacturerSpecificV2.deviceSpecificGet(deviceIdType: 1))
    return delayBetween(cmds, 200)
 }
-
-command "setLED", [[name:"ledNumber", type: "NUMBER", description: "LED/button number (1-5, 5=large/relay button)", constraints: 1..5],
-                         [name:"colorName", type: "ENUM", description: "Color name (white, blue, green, red)", constraints: ["white", "blue", "green", "red"]],
-                         [name:"brightness", type: "ENUM", description: "Brightness level (100, 60, or 30%; will round to nearest; 0 for off)", constraints: [100,60,30,0]],
-                        ] 
-
 
 List<String> setLED(Number ledNumber, String colorName, brightness) {
    if (enableDebug) log.debug "setLED(Number $ledNumber, String $colorName, Object $brightness)"
